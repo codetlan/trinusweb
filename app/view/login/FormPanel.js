beb298abb7a77e92ce20b43dfc1765c8670a8c5a
Luis Enrique Martínez Gómez <lumartineck@gmail.com>
2013-01-24 19:14:38 -0600

    [LEMG] Geolocalización

--- app/view/login/FormPanel.js
+++ app/view/login/FormPanel.js
@@ -55,10 +55,11 @@ Ext.define('App.view.login.FormPanel', {
         var form = this.getForm(),
             obj = form.getValues();
         if(form.isValid()){
-            Ext.Ajax.request({
+            /*Ext.Ajax.request({
                 scope: this,
-                url: 'proxy.php?url=http%3A%2F%2Fisystems.com.mx%3A8181%2FTrinus%2FServletLoginCliente%3Fmovil%3D'
-                    +obj.txtUser+'%26password%3D'+ obj.txtPass,
+                url: 'http://isystems.com.mx:8181/Trinus/ServletLoginCliente?movil='+obj.txtUser+'&password='+ obj.txtPass,
+                /*url: 'proxy.php?url=http%3A%2F%2Fisystems.com.mx%3A8181%2FTrinus%2FServletLoginCliente%3Fmovil%3D'
+                    +obj.txtUser+'%26password%3D'+ obj.txtPass,* /
                 success: function(response){
                     var r = Ext.decode(response.responseText);
                     if(r.result === "ok"){
@@ -68,8 +69,24 @@ Ext.define('App.view.login.FormPanel', {
                         //Notification.warn(r.error);
                     }
                 }
-            });
+            });*/
+            var invocation=new XMLHttpRequest(),
+                url = 'http://isystems.com.mx:8181/Trinus/ServletLoginCliente?movil='+obj.txtUser+'&password='+ obj.txtPass;
+            if(invocation) {
+                invocation.open('POST', url, true);
+                invocation.onreadystatechange = this.handleo.bind(this);
+                invocation.send();
+            }
+        }
+    },
 
+    handleo:function(response){
+        var r = Ext.decode(response.target.responseText);
+        if(r.result === "ok"){
+            localStorage.setItem("Logeado", 1);//Se guarda un identificador para no perder la session
+            this.fireEvent("logeado");
+        } else{
+            //Notification.warn(r.error);
         }
     }
 });
