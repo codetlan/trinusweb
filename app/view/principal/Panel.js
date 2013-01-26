e017684135b9e62d6c23d301b97e10cd9481b19c
Luis Enrique Martínez Gómez <lumartineck@gmail.com>
2013-01-25 18:44:39 -0600

    [LEMG] Se ocupa directo el mapa de google

--- app/view/principal/Panel.js
+++ app/view/principal/Panel.js
@@ -1,7 +1,7 @@
 Ext.define('App.view.principal.Panel', {
     extend: 'Ext.panel.Panel',
     alias :'widget.panelprincipal',
-    requires: ['Ext.toolbar.Toolbar','App.view.menu.MenuPanel','App.view.maps.MapPanel'],
+    requires: ['Ext.toolbar.Toolbar','App.view.menu.MenuPanel'/*,'App.view.maps.MapPanel'*/],
 
     layout:'border',
 
@@ -13,30 +13,24 @@ Ext.define('App.view.principal.Panel', {
         this.callParent(arguments);
     },
 
-    buildItems:function(){
+    buildItems:function () {
         var items = [
-            /*{
-                layout: 'form',
-                region: 'north',
-                items: [this.buildBbar()]
-            },*/
             {
-                xtype:'container',
-                layout: 'fit',
-                region : 'west',
-                flex:.2,
-                items: [{
-                    xtype: 'menupanel'
-                }]
+                xtype:'menupanel',
+                region:'west',
+                flex:.2
             },{
-                xtype:'panel',
-                layout: 'fit',
-                region : 'center',
-                flex: 1,
-                items:[{
-                    xtype: 'mappanel'
-                }]
-            }];
+                xtype:'container',
+                region:'center',
+                flex:1,
+                html:'<div id="map_canvas" style="height: 100%; width: 100%;"></div>',
+                //id:'map_canvas',
+                listeners:{
+                    scope:this,
+                    afterrender:this.onAfterRenderMapPanel
+                }
+            }
+        ];
 
         return items;
     },
@@ -44,7 +38,7 @@ Ext.define('App.view.principal.Panel', {
     buildBbar: function(){
         var toolbar =  Ext.create('Ext.toolbar.Toolbar',{
             xtype: 'container',
-            items: [{
+            items: ['->',{
                 xtype: 'button',
                 text: 'Usuario',
                 icon: 'images/user.png'
@@ -62,5 +56,62 @@ Ext.define('App.view.principal.Panel', {
     salir: function(){
         localStorage.removeItem('Logeado');
         location.href='index.html';
+    },
+
+    onAfterRenderMapPanel:function(t, eOpts){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.createMap.bind(this), this.gestionaErroresGeo);
+        } else {
+            alert('Tu navegador no soporta la API de geolocalizacion');
+        }
+    },
+
+    createMap:function (position) {
+        var _this = this,
+            mapOptions = {
+                zoom:14,
+                center:new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+
+                mapTypeId:google.maps.MapTypeId.ROADMAP
+            },
+            latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            geocoder = new google.maps.Geocoder(),
+            infowindow = new google.maps.InfoWindow();
+
+        this.map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+        geocoder.geocode({'latLng':latlng}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    _this.map.setZoom(16);
+                    var marker = new google.maps.Marker({
+                        draggable:true,
+                        position:latlng,
+                        map:_this.map
+                    });
+                    infowindow.setContent(results[0].formatted_address);
+                    infowindow.open(_this.map, marker);
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(_this.map,marker);
+                    });
+                }
+            }
+        });
+    },
+
+    gestionaErroresGeo:function (err) {
+        if (err.code == 0) {
+            alert("error desconocido");
+        }
+        if (err.code == 1) {
+            alert("El usuario no ha compartido su posicion");
+        }
+        if (err.code == 2) {
+            alert("no se puede obtener la posicion actual");
+        }
+        if (err.code == 3) {
+            alert("timeout recibiendo la posicion");
+        }
     }
 });
